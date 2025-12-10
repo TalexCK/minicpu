49d72c80a20fe4caffc107799d55e5007d0a3a28
Hao Chen <honahec@gmail.com>
2025-12-10 14:41:48 +0800

    feat: add basic config (#2)

--- spinal/hw/spinal/minicpu/Config.scala
+++ spinal/hw/spinal/minicpu/Config.scala
@@ -1,5 +1,23 @@
 package minicpu
 
-object Config {
-  
+import spinal.core._
+
+// Parameters for the RISC-V CPU
+case class RiscvConfig(
+    xlen: Int = 32, // register width (RV32I)
+    addrWidth: Int = 32,
+    resetVector: Long = 0x00000000l // PC reset value
+) {
+    // generate UInt types based on xlen and addrWidth
+    def wordType = UInt(xlen bits)
+    def addressType = UInt(addrWidth bits)
+}
+
+object ProjectConfig {
+    def spinal = SpinalConfig(
+        targetDirectory = "hw/gen",
+        defaultConfigForClockDomains = ClockDomainConfig(
+            resetActiveLevel = HIGH
+        ),
+    )
 }
