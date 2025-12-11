2394f0c849ab79ed395907b899454094d886ab7e
Hao Chen <honahec@gmail.com>
2025-12-11 22:53:10 +0800

    feat: add interfaces for CPU (#4)

---
+++ spinal/hw/spinal/minicpu/interfaces/Bus.scala
@@ -0,0 +1,36 @@
+package minicpu.interfaces
+
+import minicpu.RiscvConfig
+import spinal.lib.IMasterSlave
+import spinal.core._
+
+// define a bundle 
+case class SimpleBus(config: RiscvConfig) extends Bundle with IMasterSlave {
+    val enable = Bool() // indicates a valid transaction
+    val write = Bool() // indicates read (0) or write (1)
+    val address = config.addressType // address for the transaction
+    val writeData = config.wordType // data to write (from CPU to memory)
+    val readData = config.wordType // data read (from memory to CPU)
+
+    // define the direction of master(CPU) 
+    override def asMaster(): Unit = {
+        out(enable, write, address, writeData)
+        in(readData)
+    }
+}
+
+// define ALU operations
+object AluOp extends SpinalEnum {
+    val ADD, SUB, AND, OR, XOR, SLL, SRL, SRA, SLT, SLTU = newElement()
+}
+
+// define control signals bundle(Controller -> Datapath)
+case class ControlSignals(config: RiscvConfig) extends Bundle {
+    val regWriteEnable = Bool() // enable register write
+    val aluOp = AluOp() // ALU operation
+    val aluSrc1 = Bool() // ALU source 1 (0: register, 1: PC)
+    val aluSrc2 = Bool() // ALU source 2 (0: register, 1: immediate)
+    val memRead = Bool() // enable memory read
+    val memWrite = Bool() // enable memory write
+    val memToReg = UInt(2 bits) // select source of data (0: ALU result, 1: memory data, 2: PC + 4)
+}
