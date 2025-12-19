b04793003b49555564dc6e7c2f29b885745f9767
TalexCK <talexcks@gmail.com>
2025-12-19 22:20:10 +0800

    feat: add pc

---
+++ spinal/hw/spinal/minicpu/components/Pc.scala
@@ -0,0 +1,21 @@
+package minicpu.components
+
+import minicpu.RiscvConfig
+import spinal.core._
+
+class Pc (config: RiscvConfig) extends Component {
+  val io = new Bundle {
+    val pcNext = in(config.addressType)
+    val we = in(Bool())
+    val pc = out(config.addressType)
+  }
+
+  val initValue = U(config.resetVector, config.addrWidth bits)
+  val reg = RegInit(initValue)
+
+  when(io.we){
+    reg := io.pcNext
+  }
+  io.pc := reg
+
+}
