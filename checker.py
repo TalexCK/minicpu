eb529be44c1712345599398f2fe438595e8f66f9
TalexCK <talexcks@gmail.com>
2026-01-10 00:47:50 +0800

    fix: correct typo in MiniCPU print statement

--- checker.py
+++ checker.py
@@ -286,7 +286,7 @@ def compare_logs(base_dir: Path) -> None:
         if x.strip()
     ]
 
-    print("[-] MiniCpu Final Regs: " + a_lines[-1])
+    print("[-] MiniCPU Final Regs: " + a_lines[-1])
     print("[-] Spike Final Regs: " + b_lines[-1])
     a_parsed = [parse_dump_line(x) for x in a_lines]
     b_parsed = [parse_dump_line(x) for x in b_lines]
